7344b82e602099a442baec86eeae64cb62ea1adf
Martin Lam <martinlam16061@gmail.com>
2026-06-19 11:15:50 -0700

    got rid of dupicate hobbies section

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 from app.education import education_bp
-from app.hobbies import hobbies_bp
 
 load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(education_bp)
-app.register_blueprint(hobbies_bp)
 
 
 @app.route('/')
