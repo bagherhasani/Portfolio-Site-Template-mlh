6ade8d51354e50f7e31ba71e6896b2f7e0778f06
bagherhasani0 <bah3944@utulsa.edu>
2026-07-12 15:06:40 -0500

    feature / ui is final commit before merging

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,45 @@
+from crypt import methods
+import datetime
+from email.policy import default
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-
+from peewee import *
+import datetime
 from app.education import education_bp
+from playhouse.shortcuts import model_to_dict
+from pathlib import Path
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / "example.env")
 app = Flask(__name__)
 app.register_blueprint(education_bp)
+
+
+#database connection here 
+mydb=MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+user=os.getenv("MYSQL_USER"),
+password=os.getenv("MYSQL_PASSWORD"),
+host=os.getenv("MYSQL_HOST"),
+port=3306
+
+)
+print(mydb)
+
+#database table model
+class TimelinePost(Model):
+    name=CharField()
+    email=CharField()
+    content=TextField()
+    created_at=DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database=mydb
+    
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+
 
 WORK_EXPERIENCE_ENTRIES = [
     {
@@ -128,3 +161,24 @@ def map():
 @app.route('/hobbies')
 def hobbies():
     return render_template('hobbies.html', hobbies=HOBBIES)
+
+
+
+@app.route('/api/timeline_post',methods=['POST'])
+def post_time_list_post():
+    name=request.form['name']
+    email=request.form['email']
+    content=request.form['content']
+    timeline_post=TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
