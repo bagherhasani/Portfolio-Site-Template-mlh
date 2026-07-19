4db74bc372f6805673d59145e9da55b4c77c6f51
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-07-19 12:38:49 -0600

    updated POSTin order to protect against malformed data

--- app/__init__.py
+++ app/__init__.py
@@ -173,9 +173,15 @@ def hobbies():
 
 @app.route('/api/timeline_post',methods=['POST'])
 def post_time_list_post():
-    name=request.form['name']
-    email=request.form['email']
-    content=request.form['content']
+    name=request.form.get('name')
+    if not name: 
+        return "Invalid Name", 400
+    email=request.form.get('email')
+    if not email or '@' not in email: 
+        return "Invalid Email", 400
+    content=request.form.get('content')
+    if not content: 
+        return "Invalid Content", 400
     timeline_post=TimelinePost.create(name=name, email=email, content=content)
 
     return model_to_dict(timeline_post)
